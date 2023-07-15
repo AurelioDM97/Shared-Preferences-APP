b631a183893939aeb1d8840abf98892365b27967
AurelioDM97 <126873779+AurelioDM97@users.noreply.github.com>
2023-07-15 11:31:40 +0200

    added shared preferences for saving username

--- app/src/main/java/com/example/sharedpreferencesapp/MainActivity.kt
+++ app/src/main/java/com/example/sharedpreferencesapp/MainActivity.kt
@@ -1,11 +1,56 @@
 package com.example.sharedpreferencesapp
 
+import android.annotation.SuppressLint
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import android.widget.Toast
+import com.example.sharedpreferencesapp.databinding.ActivityMainBinding
+import io.github.muddz.styleabletoast.StyleableToast
 
+const val SHARED_PREF = "userName"
+const val SAVE_USER = "userName"
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val button = binding.button
+
+        loadId()
+
+        button.setOnClickListener {
+            saveId()
+
+            val toast = Toast.makeText(this, R.string.hai_cambiato_id_correttamente, Toast.LENGTH_SHORT)
+            val toastLayout = layoutInflater.inflate(R.layout.custom_toast, null)
+            toast.view = toastLayout
+            toast.setGravity(Gravity.BOTTOM, 0, 25)
+
+            toast.show()
+        }
+    }
+    private fun loadId() {
+        val textId = binding.id
+        val sharedPrefs = getSharedPreferences(SHARED_PREF, Context.MODE_PRIVATE)
+        val saveId = sharedPrefs.getString(SAVE_USER, null)
+        textId.text = saveId
+    }
+    private fun saveId() {
+        val editTextId = binding.editText
+        val textId = binding.id
+        val inputId = editTextId.text.toString()
+        textId.text = inputId
+
+        val sharedPrefs = getSharedPreferences(SHARED_PREF, Context.MODE_PRIVATE)
+
+        sharedPrefs.edit()
+            .putString(SAVE_USER, inputId)
+            .apply()
     }
 }
